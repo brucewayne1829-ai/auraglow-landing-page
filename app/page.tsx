28d7dbc8aa9f9fa1b588f780f67762497f7ceace
brucewayne1829-ai <brucewayne1829@gmail.com>
2026-07-27 10:46:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import { Benefits } from "@/components/store/benefits"
-import { Faq } from "@/components/store/faq"
 import { Hero } from "@/components/store/hero"
-import { HowToUse } from "@/components/store/how-to-use"
 import { OrderDialog } from "@/components/store/order-dialog"
 import { OrderProvider } from "@/components/store/order-context"
-import { Recipes } from "@/components/store/recipes"
-import { Reviews } from "@/components/store/reviews"
 import { SiteFooter } from "@/components/store/site-footer"
 import { SiteHeader } from "@/components/store/site-header"
 import { StickyBuyBar } from "@/components/store/sticky-buy-bar"
@@ -16,11 +11,6 @@ export default function Page() {
       <SiteHeader />
       <main className="pb-24 md:pb-0">
         <Hero />
-        <HowToUse />
-        <Benefits />
-        <Recipes />
-        <Reviews />
-        <Faq />
       </main>
       <SiteFooter />
       <StickyBuyBar />
